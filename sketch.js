94e2d7a3697b81b73c0cfc573e0e0738e658b896
lexsino <104579384+lexsino@users.noreply.github.com>
2022-04-29 11:15:01 -0400

    initial files

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//variables
+//create variable called x with value 0
+let x = 0
+let y = 0
+let ballColor =
+
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
